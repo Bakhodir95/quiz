6f1c10999e5b2ce49861a1e47135806b77b13319
Bahodir <bakhodir.turdimurodov@bk.ru>
2024-05-01 09:30:48 +0500

    answer

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/main_page.dart';
+
+void main(List<String> args) {
+  runApp(Quiz());
+}
+
+class Quiz extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainApp());
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MainApp();
+  }
+}
+
+class _MainApp extends State<MainApp> {
+  int count = 1;
+  String answer = "-savol";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz app"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$count$answer",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      if (count > 1) {
+                        count--;
+                      }
+                      setState(() {});
+                    },
+                    child: Text(
+                      "Back",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      count++;
+                      setState(() {});
+                    },
+                    child: Text(
+                      "Next",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
